2f64c19d99bc8aecc84972e0cbafab8408375a25
nrana <rananitish37@gmail.com>
2025-08-24 10:30:54 +0530

    modified

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 /*========typing animation======== */
 
 var typed = new Typed(".typing",{
-    strings:["Full Stack Developer","Web Developer","Android Developer","Web Designer"],
+    strings:["Full Stack Developer","Web Developer","Android Developer"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
--- js/sendmail.js
+++ js/sendmail.js
@@ -1,13 +1,13 @@
 function sendMail(){
-    Email.send({
-        Host : "smtp.gmail.com",
-        Username : "rananitish129@gmail.com",
-        Password : "Nitish@1234",
-        To : 'belle76348@gmail.com',
-        From : document.getElementById("email").value,
-        Subject : document.getElementsById("subject").value,
-        Body : document.getElementById("msg").value,
-    }).then(
-      message => alert(message)
-    ); 
+  Email.send({
+      Host : "smtp.gmail.com",
+      Username : "rananitish129@gmail.com",
+      Password : "Nitish@1234",
+      To : 'belle76348@gmail.com',
+      From : document.getElementById("email").value,
+      Subject : document.getElementById("subject").value, // Corrected syntax here
+      Body : document.getElementById("msg").value,
+  }).then(
+    message => alert(message)
+  );
 }
